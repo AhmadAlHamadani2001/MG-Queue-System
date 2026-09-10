1d72b208fa9f9fdea6e24f05edec93f72c1f9b1c
Ahmad Hamdani <ahmad.hamdani@mynaghi.com>
2026-09-11 00:26:15 +0300

    Fix build error: type cast for Uint8Array applicationServerKey

--- lib/pushSubscribe.ts
+++ lib/pushSubscribe.ts
@@ -35,7 +35,7 @@ export async function subscribeToPush(ticketId: string, mobile: string): Promise
     if (!subscription) {
       subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as BufferSource,
       });
     }
 
